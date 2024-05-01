832952b4ceb9a4a8d250b6acad46d2c01f801a67
BbillahrariBBr <mb.billah8@gmail.com>
2024-05-01 09:25:30 +0600

    Start Level 2 first mod

---
+++ CrushNodeExpressMongo/Module1/1-9ExploreCommonJs/file1.js
@@ -0,0 +1,5 @@
+const {a,add,b} = require("./file2.js");
+
+console.log(add(4, 3));
+console.log(b);
+console.log(a);
---
+++ CrushNodeExpressMongo/Module1/1-9ExploreCommonJs/file2.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 12;
+
+const add = (param1, param2) => param1 + param2;
+
+module.exports = {
+  a,
+  add,
+  b,
+};
+//console.log(module);
