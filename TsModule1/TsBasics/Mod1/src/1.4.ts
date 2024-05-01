7818dcb2d21e79aadaf2d41848e52b5f1139c905
BbillahrariBBr <mb.billah8@gmail.com>
2024-05-01 09:46:16 +0600

    end 1.4 Basic Data types

---
+++ TsModule1/TsBasics/Mod1/src/1.4.ts
@@ -0,0 +1,29 @@
+//basic data type
+// string
+let firstName: string = "Mezba";
+//number
+let roll: number = 123;
+
+//bool
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+//null
+let y: null = null;
+
+//non premitive
+
+//array
+let friends: string[] = ["rachel", "tsetka"];
+friends.push('hello');
+
+let eligibleList: number[] = [1, 2];
+
+eligibleList.push(4);
+
+// tuple -- > array --> oder --> type of value
+
+let coordinate: [number, number] = [1, 5];
+let ageName: [number, string] = [50, "Mr. X"];
+ageName[0] = 403;
