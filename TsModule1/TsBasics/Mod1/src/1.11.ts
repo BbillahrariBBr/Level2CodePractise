2b51b4f1aa27bcdfb775b3fce7c0ac40825dd109
BbillahrariBBr <mb.billah8@gmail.com>
2024-05-01 12:01:27 +0600

    end 1.11 done

---
+++ TsModule1/TsBasics/Mod1/src/1.11.ts
@@ -0,0 +1,33 @@
+{
+
+    //ternary, optional, nullish coalescing operator
+
+    // install - npm i -g ts-node-dev
+    // tsnd --respawn server.ts
+    //ts-node-dev --respawn --transpile-only server.ts
+
+
+    const age: number = 18;
+
+    // if(age>=18)
+    //     {
+    //         console.log("adult")
+    //     }
+    //     else{
+    //         console.log("non Adult")
+    //     }
+
+const isAdult = age >=18 ? "Adult":"Child"
+console.log(isAdult)
+
+//nullish coalescing oper
+// null / undefined --> decission amking
+
+const isAuthenticated = null;
+const res1 = isAuthenticated ?? "Guest"
+console.log(res1)
+
+
+
+    //
+}
