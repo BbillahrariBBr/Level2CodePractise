f12dce81615ba7d553be08ca2b0355e52378975a
BbillahrariBBr <mb.billah8@gmail.com>
2024-05-01 11:08:13 +0600

    1.8 done

---
+++ TsModule1/TsBasics/Mod1/dist/1.8.js
@@ -0,0 +1,18 @@
+"use strict";
+{
+    // object destructuring
+    const user = {
+        id: 345,
+        name: {
+            firstName: 'Nannu',
+            middleName: 'Khan',
+            LastName: 'kichuna'
+        },
+        cno: '0123465233',
+        address: "Uganda"
+    };
+    const { cno, name: { middleName } } = user;
+    // array destructuring
+    const myFriends = ["Charli", "Joye", "Ross", "Rachel", "Monica"];
+    const [, , bestF, ...rest] = myFriends;
+}
