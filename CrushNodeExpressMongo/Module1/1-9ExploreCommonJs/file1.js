1e6102c3daefdb84a68afcb3620fd7ccb080c1e0
BbillahrariBBr <mb.billah8@gmail.com>
2024-05-02 12:33:31 +0600

    1-10 Name Exports, Name Aliasing & Index Export

--- CrushNodeExpressMongo/Module1/1-9ExploreCommonJs/file1.js
+++ CrushNodeExpressMongo/Module1/1-9ExploreCommonJs/file1.js
@@ -1,5 +1,6 @@
-const {a,add,b} = require("./file2.js");
+const { a, add, b } = require("./file2.js");
+const { a: a3, add: add3, b: b3 } = require("./file3.js");
 
-console.log(add(4, 3));
-console.log(b);
-console.log(a);
+console.log(add3(4, 3, 5));
+console.log(b3);
+console.log(a3);
---
+++ CrushNodeExpressMongo/Module1/1-9ExploreCommonJs/main.js
@@ -0,0 +1,7 @@
+// const { add } = require("./utils/add.js");
+// const { sub } = require("./utils/sub.js");
+
+const { add, sub } = require("./utils/index");
+
+console.log("add:", add(5, 3));
+console.log("sub:", sub(4, 3));
