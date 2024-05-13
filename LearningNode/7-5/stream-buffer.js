4a22e10f1c2c6b80fe0b0e4226de8a7db03434d7
BbillahrariBBr <mb.billah8@gmail.com>
2024-05-14 05:42:39 +0600

    done basic njs

--- LearningNode/7-5/stream-buffer.js
+++ LearningNode/7-5/stream-buffer.js
@@ -6,16 +6,23 @@ const serv = http.createServer();
 // lsitener
 serv.on("request", (req, res) => {
   if (req.url === "./read-file" && req.method === "GET");
-    //streaming file reading
-    const readableStream = fs.createReadStream("../7-3/text/read.txt");
-    readableStream.on("data", (buffer) => {
-      res.write(buffer);
-    });
+  //streaming file reading
+  const readableStream = fs.createReadStream("../7-3/text/read.txt");
+  readableStream.on("data", (buffer) => {
+    res.statusCode = 200;
+    res.write(buffer);
+  });
 
-    readableStream.on("end", () => {
-      res.end("Hello From World!");
-    });
-  
+  readableStream.on("end", () => {
+    res.statusCode = 200;
+    res.end("The Streaming is Over!");
+  });
+
+  readableStream.on("error", (error) => {
+    console.log(error);
+    res.statusCode = 500;
+    res.end("Something is went wrong!");
+  });
 });
 // Create a local server to receive data from
 // const server = http.createServer((req, res) => {
