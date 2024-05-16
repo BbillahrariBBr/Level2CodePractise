cca5349d9be858b8bc6e4444e3e8020fc430cd78
BbillahrariBBr <mb.billah8@gmail.com>
2024-05-16 23:38:08 +0600

    8-3 Installing Eslint, Refactor Code, Fix Errors Using Command 8 .36

--- Module8/first-project/src/app.ts
+++ Module8/first-project/src/app.ts
@@ -1,11 +1,18 @@
-const express = require("express");
-const app = express();
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+//parser
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default {
+  app,
+};
+
+console.log(process.cwd());
